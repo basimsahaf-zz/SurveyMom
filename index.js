95d3bd963025e05b696b43c57435c11e619451e8
basimsahaf <bssahaf@uwaterloo.ca>
2017-08-17 23:25:57 -0400

    Refactored code, added google authentication, added callback

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 /* Constants */
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport.js');
+
 
 const app = express();
 
-//tells the passport how to implement GoogleStrategy
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken)=> {
-    console.log(accessToken);
-}));
+require('./routes/authRoutes')(app);
 
-
-//route handler
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
 
 //Environment variable injected by Heroku, uses port 5000 if environment variable is missing
 /* =====================================================================*/
